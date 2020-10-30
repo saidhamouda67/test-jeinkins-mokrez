5add8e19f56bab9e0ab34ecae29659599efd69c6
medsaidraoudh <saidhamouda67@yahoo.fr>
2020-10-30 12:48:43 +0100

    added sonar and test junit and log4j

--- src/test/java/tn/esprit/spring/TimesheetApplicationTests.java
+++ src/test/java/tn/esprit/spring/TimesheetApplicationTests.java
@@ -1,13 +1,29 @@
 package tn.esprit.spring;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+import org.apache.log4j.Logger;
 class TimesheetApplicationTests {
-
+private static final Logger l = Logger.getLogger(TimesheetApplicationTests.class);
 	@Test
 	void contextLoads() {
+			l.info("starting contextLoads : ");
+			l.debug("Je vais lancer la divsion.");
+		try {
+
+		
+			int i = 1/0;
+			l.debug("Je viens de lancer la divsion. " + i);
+			l.debug("Je viens de finir l'opération X.");
+			l.info("Out getAllPrducts() without errors.");
+}
+catch (Exception e) { 
+	l.error("Erreur dans contextLoads : " + e);
+ }
+		int a=33;
+		int b=33;
+		assertEquals(a,b,"yeaah its the same");
 	}
 
 }
